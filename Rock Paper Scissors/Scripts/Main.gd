b533c17a3c0d6bed66b18de876b54cbaefe3d05c
Una Adilović <adilovicuna@gmail.com>
2023-04-03 21:29:46 +0200

    UI connected

--- Rock Paper Scissors/Scripts/Main.gd
+++ Rock Paper Scissors/Scripts/Main.gd
@@ -1,18 +1,53 @@
 extends Node2D
 
+onready var menu = get_node("UI/MainMenu")
+onready var restart = get_node("UI/Restart")
+
 # screen size = 1000x600
+
 
 var base_players = ["Rock", "Paper", "Scissors"]
 var extended_players = ["Lizard", "Spock"]
 var scenes = []
 
-var extended = true
-
 var rand = RandomNumberGenerator.new()
 var instances
 
-func _ready():
-    var spawn = 200
+var spawn = 100
+var extended = false
+
+var generated = false
+
+func _physics_process(_delta):
+    if generated and all_same():
+        $UI/Restart/WinMessage.text = get_type(instances.get_child(0).name) + " Won!"
+        generated = false
+        for instance in instances.get_children():
+            instance.queue_free()
+        restart.show()
+
+func all_same():
+    var first_child = instances.get_child(0)
+    var type = get_type(first_child.name)
+        
+    for instance in instances.get_children():
+        if get_type(instance.name) != type:
+            return false
+    return true
+        
+func get_type(name):
+    if "Rock" in name:
+        return "Rock"
+    if "Paper" in name:
+        return "Paper"
+    if "Scissors" in name:
+        return "Scissors"
+    if "Lizard" in name:
+        return "Lizard"
+    if "Spock" in name:
+        return "Spock"
+
+func generate():
     instances = get_child(2)
     
     for name in base_players:
@@ -30,3 +65,20 @@ func _ready():
             
     for instance in instances.get_children():
         instance.set_velocity()
+
+func _on_Start_pressed():
+    menu.hide()
+    generate()
+    generated = true
+
+func _on_Size_value_changed(value):
+    spawn = value
+
+func _on_ExtendedCheckbox_toggled(button_pressed):
+    extended = button_pressed
+
+func _on_Restart_pressed():
+    restart.hide()
+    menu.show()
+
+
